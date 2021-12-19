5b24a9cb15bf536e0bd33ceac3faa3cc9cb05df6
dinmohammad2505 <dinmohammad2505@gmail.com>
2021-12-19 15:27:18 +0600

    updated

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#counter').parallax({
+        speedFactor: .4,
+        offsetX: "50%",
+        offsetY: 0,
+
+    });
+    
+        $('#counter2').parallax({
+        speedFactor: .4,
+        offsetX: "50%",
+        offsetY: 0,
+
+        });
+    
+     new WOW().init();
+}); 
